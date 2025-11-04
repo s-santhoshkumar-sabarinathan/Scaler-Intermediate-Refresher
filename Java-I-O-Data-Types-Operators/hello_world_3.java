2f0d4cac6dc6a95bc143c8d37c1e125b0d1a9065
santhoshkumar <santhoshkumar.sabarinathan@gmail.com>
2025-11-04 10:16:23 +0530

    Modify Q1 and Q2 Add Q3

---
+++ Java-I-O-Data-Types-Operators/hello_world_3.java
@@ -0,0 +1,24 @@
+/*
+Problem Description
+
+Print the following text in the output:
+Hello
+
+World !
+Note: There is a empty line you need print between Hello and World !
+
+Output Format
+Print the required text in the output.
+
+Example Output
+Hello
+World !
+ */
+
+class HelloWorldQ3 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println();
+        System.out.println("World");
+    }
+}
