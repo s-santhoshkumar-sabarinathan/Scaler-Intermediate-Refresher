cadfc112f4315fd42ebb2dd7a4f0acb52507c621
santhoshkumar <santhoshkumar.sabarinathan@gmail.com>
2025-11-04 10:10:26 +0530

    Q2

---
+++ Java-I-O-Data-Types-Operators/HelloWorld2.java
@@ -0,0 +1,22 @@
+/*
+Problem Description
+
+Print the following text:
+Hello 40
+50 World !
+
+Output Format
+Print the output specifed in the problem description.
+
+Example Output
+Hello 40
+50 World !
+
+ */
+
+class HelloWorld2 {
+    public static void main(String[] args) {
+        System.out.println("Hello 40");
+        System.out.println("50 World !");
+    }
+}
